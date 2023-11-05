9c077637177616bc65c61941706a4cfcebf01c0f
Karina <erohovets00@gmail.com>
2023-11-05 14:11:19 +0500

    Добавление авторизации и регистрации пользователя

---
+++ src/main/java/com/project/passmanager/main/registration/controllers/RegistrationController.java
@@ -0,0 +1,39 @@
+package com.project.passmanager.main.registration.controllers;
+
+import com.project.passmanager.main.registration.models.UserRegistration;
+import com.project.passmanager.main.registration.services.UserRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegistrationController {
+    private final UserRegistrationService userRegistrationService;
+
+    @Autowired
+    public RegistrationController(UserRegistrationService userRegistrationService) {
+        this.userRegistrationService = userRegistrationService;
+    }
+    @GetMapping("/registration")
+    public String registration()
+    {
+        return "registration";
+    }
+    @PostMapping("/registration")
+    public String adduser(UserRegistration userRegistration, Model model)
+    {
+        try
+        {
+            userRegistrationService.addUser(userRegistration);
+            return "redirect:/login";
+        }
+        catch (Exception ex)
+        {
+            model.addAttribute("message", "UserRegistration exists");
+            return "registration";
+        }
+    }
+
+}
---
+++ src/main/java/com/project/passmanager/main/registration/repositories/UserRegistrationRepository.java
@@ -0,0 +1,8 @@
+package com.project.passmanager.main.registration.repositories;
+
+import com.project.passmanager.main.registration.models.UserRegistration;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRegistrationRepository extends CrudRepository<UserRegistration, Long> {
+    UserRegistration findByUsername(String username);
+}
